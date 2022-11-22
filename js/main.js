01b5b33c2879b742589c0a5ebe0647c2211eba7d
Bruno Pezman <brunopezman@gmail.com>
2022-11-22 15:28:57 -0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,78 @@
+// Primer mensaje de página
+alert("Bienvenido a Rock Merch N'Roll" )
+
+//simulador de compra de productos
+
+// Variables inicializadas 
+let nombreIngresado = "" .toUpperCase;
+let userEmail = "";
+let confirmUserEmail = "";
+let askAgain = true;
+let producto = "" .toUpperCase;
+let precio = 0;
+let cantidad = 0;
+let precioTotal = 0;
+let cantidadTotal = 0;
+let seguirComprando = false;
+
+//funciones
+const multiplicacion = (a,b) => a * b ;
+const suma = (a,b) => a + b ;
+
+// Identificación del usuario
+do {
+    nombreIngresado = prompt("Ingrese su nombre");
+
+    if (nombreIngresado == ""){
+        alert("Vuelva a ingresar su nombre porfavor")
+    }else {
+        askAgain = false;
+    }
+
+} while (askAgain);
+
+do {
+    userEmail = prompt("Ingrese su correo electrónico para enviarle posteriormente todos los detalles de la compra");
+    confirmUserEmail = prompt("confirme su correo electrónico");
+
+    if (userEmail === confirmUserEmail) {
+        alert("Los datos han sido guardados con éxito")
+        askAgain = false;
+    }else{
+        alert("Los correos electronicos deben ser iguales");
+    }
+
+} while (askAgain);
+
+
+//Operación de compra
+do {
+    producto = prompt("¿Desea comprar remeras, buzos o accesorios?");
+    cantidad = parseInt (prompt("Ingrese cantidad de productos que desea comprar"));
+
+    switch(producto){
+        case "remeras":
+            precio = 2500;
+            break
+        case "buzos":
+            precio = 4000;
+            break
+        case "accesorios":
+            precio = 1200;
+            break
+        default:
+            alert("El producto no esta en nuestro catálogo");
+            precio = 0;
+            cantidad = 0;
+            break;
+    }   
+
+    precioTotal += multiplicacion (precio, cantidad);
+    cantidadTotal += cantidad;
+    seguirComprando = confirm("¿Desea seguir comprando?");
+
+} while (seguirComprando);
+
+// Detalles de la compra
+alert("Compraste "+cantidadTotal+" productos y el total de su compra es "+precioTotal);
+alert("Gracias "+nombreIngresado+" por confiar en nosotros");
