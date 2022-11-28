0675799f5c2dd5d067b720f643302c666793d848
Bruno Pezman <brunopezman@gmail.com>
2022-11-28 20:33:35 -0300

    Corrigiendo con funciones

--- js/main.js
+++ js/main.js
@@ -4,78 +4,169 @@ alert("Bienvenido a Rock Merch N'Roll" )
 // Simulador de compra de productos
 
 // Variables inicializadas 
-let nombreIngresado = "" .toUpperCase;
-let userEmail = "";
-let confirmUserEmail = "";
-let askAgain = true;
-let producto = "" .toUpperCase;
-let precio = 0;
-let cantidad = 0;
-let precioTotal = 0;
-let cantidadTotal = 0;
-let seguirComprando = false;
+// let nombreIngresado = "" .toUpperCase;
+// let userEmail = "";
+// let confirmUserEmail = "";
+// let askAgain = true;
+// let producto = "" .toUpperCase;
+// let precio = 0;
+// let cantidad = 0;
+// let precioTotal = 0;
+// let cantidadTotal = 0;
+// let seguirComprando = false;
 
 // Funciones 
-const multiplicacion = (a,b) => a * b ;
-const suma = (a,b) => a + b ;
+// const multiplicacion = (a,b) => a * b ;
+// const suma = (a,b) => a + b ;
 
 // Identificación del usuario
-do {
-    nombreIngresado = prompt("Ingrese su nombre");
+// do {
+//     nombreIngresado = prompt("Ingrese su nombre");
 
-    if (nombreIngresado == ""){
-        alert("Vuelva a ingresar su nombre porfavor")
-    }else {
-        askAgain = false;
-    }
+//     if (nombreIngresado == ""){
+//         alert("Vuelva a ingresar su nombre porfavor")
+//     }else {
+//         askAgain = false;
+//     }
 
-} while (askAgain);
+// } while (askAgain);
 
-do {
-    userEmail = prompt("Ingrese su correo electrónico para enviarle posteriormente todos los detalles de la compra");
-    confirmUserEmail = prompt("confirme su correo electrónico");
+// do {
+//     userEmail = prompt("Ingrese su correo electrónico para enviarle posteriormente todos los detalles de la compra");
+//     confirmUserEmail = prompt("confirme su correo electrónico");
 
-    if (userEmail === confirmUserEmail) {
-        alert("Los datos han sido guardados con éxito")
-        askAgain = false;
-    }else{
-        alert("Los correos electronicos deben ser iguales");
-        userEmail = prompt("Ingrese nuevamente su correo electrónico");
-        confirmUserEmail = prompt("confirme su correo electrónico");
-    }
+//     if (userEmail === confirmUserEmail) {
+//         alert("Los datos han sido guardados con éxito")
+//         askAgain = false;
+//     }else{
+//         alert("Los correos electronicos deben ser iguales");
+//         userEmail = prompt("Ingrese nuevamente su correo electrónico");
+//         confirmUserEmail = prompt("confirme su correo electrónico");
+//     }
 
-} while (askAgain);
+// } while (askAgain);
 
 
 // Operación de compra
-do {
-    producto = prompt("¿Desea comprar remeras, buzos o accesorios?");
+// do {
+//     producto = prompt("¿Desea comprar remeras, buzos o accesorios?");
     
-    switch(producto){
-        case "remeras":
-            precio = 2500;
-            cantidad = parseInt (prompt("Ingrese cantidad de productos que desea comprar"));
-            break
-        case "buzos":
-            precio = 4000;
-            cantidad = parseInt (prompt("Ingrese cantidad de productos que desea comprar"));
-            break
-        case "accesorios":
-            precio = 1200;
-            break
-        default:
-            alert("El producto no esta en nuestro catálogo");
-            precio = 0
-            cantidad = 0
-            break;
-    }   
+//     switch(producto){
+//         case "remeras":
+//             precio = 2500;
+//             cantidad = parseInt (prompt("Ingrese cantidad de productos que desea comprar"));
+//             break
+//         case "buzos":
+//             precio = 4000;
+//             cantidad = parseInt (prompt("Ingrese cantidad de productos que desea comprar"));
+//             break
+//         case "accesorios":
+//             precio = 1200;
+//             break
+//         default:
+//             alert("El producto no esta en nuestro catálogo");
+//             precio = 0
+//             cantidad = 0
+//             break;
+//     }   
 
-    precioTotal += multiplicacion (precio, cantidad);
-    cantidadTotal += cantidad;
-    seguirComprando = confirm("¿Desea seguir comprando?");
+//     precioTotal += multiplicacion (precio, cantidad);
+//     cantidadTotal += cantidad;
+//     seguirComprando = confirm("¿Desea seguir comprando?");
 
-} while (seguirComprando);
+// } while (seguirComprando);
 
 // Detalles de la compra
-alert("Compraste "+cantidadTotal+" productos y el total de su compra es "+precioTotal);
-alert("Gracias "+nombreIngresado+" por confiar en nosotros");
+// alert("Compraste "+cantidadTotal+" productos y el total de su compra es "+precioTotal);
+// alert("Gracias "+nombreIngresado+" por confiar en nosotros");
+
+// Identificación del usuario
+const IdentificaciónUsuario = () => {
+
+    let nombreIngresado = "" .toUpperCase;
+    let userEmail = "";
+    let confirmUserEmail = "";
+    let askAgain = true;
+
+    do {
+        nombreIngresado = prompt("Ingrese su nombre");
+
+        if (nombreIngresado == ""){
+            alert("Vuelva a ingresar su nombre porfavor")
+        }else {         askAgain = false;
+           }
+    } while (askAgain);
+
+    do {
+        userEmail = prompt("Ingrese su correo electrónico para enviarle posteriormente todos los detalles de la compra");
+        confirmUserEmail = prompt("confirme su correo electrónico");
+   
+        if (userEmail === confirmUserEmail) {
+            alert("Los datos han sido guardados con éxito")
+            askAgain = false;
+       }else{
+            alert("Los correos electronicos deben ser iguales");
+           userEmail = prompt("Ingrese nuevamente su correo electrónico:");
+            confirmUserEmail = prompt("confirme su correo electrónico:");
+       }
+   
+   } while (askAgain);
+}
+
+IdentificaciónUsuario();
+
+// Operación de compra
+
+const operacionCompra = () => {
+    let producto = "" .toUpperCase;
+    let precio = 0;
+    let cantidad = 0;
+    let precioTotal = 0;
+    let seguirComprando = false;
+
+    do {
+        
+        producto = prompt("¿Desea comprar remeras, buzos o accesorios?");
+        
+        let cantidadValidada = validarCantidad(cantidad);
+
+        switch(producto){
+            case "remeras":
+               precio = 2500;
+               cantidad = parseInt (prompt("Ingrese la cantidad de productos que desea comprar:"));
+              break
+           case "buzos":
+                precio = 4000;
+                cantidad = parseInt (prompt("Ingrese la cantidad de productos que desea comprar:"));
+               break
+            case "accesorios":
+                precio = 1200;
+                break
+           default:
+               alert("El producto no esta en nuestro catálogo");
+                precio = 0
+                cantidad = 0
+                break;
+        }   
+
+        precioTotal += precio * cantidadValidada;
+        seguirComprando = confirm("¿Querés agregar otro producto?");
+
+
+    } while (seguirComprando);
+}
+
+// Validar cantidad puesta por el usuario
+
+const validarCantidad = (cantidad) => {
+    while (Number.isNaN(cantidad) || cantidad === 0){
+        if (cantidad != 0){
+            alert("Debe agregar un número.")
+        }else{
+            alert("Debe agregar una cantidad de producto.")
+        }
+        cantidad = parseInt(prompt("Ingrese la cantidad de productos que desea comprar:"))
+    }
+    
+    return cantidad;
+}; 
