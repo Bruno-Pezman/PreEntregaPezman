1692e9670063d2c1afb313085bcb189689997334
Bruno Pezman <brunopezman@gmail.com>
2022-12-04 17:22:49 -0300

    Modificando el stock

--- js/stock.js
+++ js/stock.js
@@ -1,50 +1,98 @@
 const remeras = [
 
     {
-        nombre: "Remera - The Beatles - negra - lisa",
-        precio: "2400"
+        id: 1,
+        nombre: "Remera The Beatles",
+        tipo: "remera",
+        descripcion: "The Beatles - negra - lisa",
+        precio: 2400,
+        talle: "L",
+        cantidad:0
     },
     {
-        nombre: "Remera - AC/DC - Negra - lisa ",
-        precio: "2400"
+        id: 2,
+        nombre: "Remera AC/DC",
+        tipo: "remera",
+        descripcion: "AC/DC - Negra - lisa",
+        precio: 2400,
+        talle: "L",
+        cantidad:0
     },
     {
-        nombre: "Remera - Led Zeppelin - negra - estampado",
-        precio: "2400"
+        id:3,
+        nombre: "Remera Led Zepellin",
+        tipo: "remera",
+        descripcion:"Led Zeppelin - negra - estampado",
+        precio: 2800,
+        talle: "M",
+        cantidad:0
     },
     {
-        nombre: "Remera - Bon Jovi - azul marino - lisa",
-        precio: "2400"
+        id:4,
+        nombre: "Remera Bon Jovi",
+        tipo: "remera",
+        descripcion:"Bon Jovi - azul marino - lisa",
+        precio: 2400,
+        talle: "M",
+        cantidad:0
     },
     {
-        nombre: "Remera - Pink Floyd - gris oscuro - estampado",
-        precio: "2400"
+        id:5,
+        nombre: "Remera Pink Floyd",
+        tipo: "remera",
+        descripcion:"Pink Floyd - gris oscuro - estampado",
+        precio: 2800,
+        talle: "L",
+        cantidad:0
     },
     {
-        nombre: "Remera - The doors - negra - estampado",
-        precio: "2400"
+        id:6,
+        nombre: "Remera The Doors",
+        tipo: "remera",
+        descripcion:"The Doors - negra - estampado",
+        precio: 2800,
+        talle: "S",
+        cantidad:0
     }
 ]
 const buzos = [
     
     {
-        nombre: "Buzo - AC/DC - negro - liso",
-        precio: "3400"
+        id:7,
+        nombre: "Buzo AC/DC",
+        tipo: "buzo",
+        descripcion:"AC/DC - negro - liso",
+        precio: 3400,
+        talle: "M",
+        cantidad:0
     },
     {
-        nombre: "Buzo - Rolling Stones - negro con gris",
-        precio: "3400"
+        id:8,
+        nombre: "Buzo Rolling Stones",
+        tipo: "buzo",
+        descripcion:"Rolling Stones - negro con gris",
+        precio: 3400,
+        talle: "M",
+        cantidad:0
     }
 ]
 const accesorios = [
 
     {
-        nombre: "gorro - Nirvana - blanca - estampado",
-        precio: "1200"
+        id:9,
+        nombre: "Gorro Nirvana",
+        tipo: "gorro",
+        descripcion:"Nirvana - blanco - estampado",
+        precio: 1200,
+        cantidad:0
     },
     {
-        nombre: "gorro - Red Hot Chili Peppers - blanca con negro - estampado",
-        precio: "1200"
+        id:9,
+        nombre: "Gorro Red Hot Chili Peppers",
+        tipo: "gorro",
+        descripcion:"Red Hot Chili Peppers - blanca con negro - estampado",
+        precio: 1200,
+        cantidad:0
     }
 ]
 
@@ -53,5 +101,3 @@ const productos = [
     remeras, buzos, accesorios
 ];
 
-console.log(productos);
-    
