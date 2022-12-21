f9fdef395d91519d626c44bc1a6bf2eab1e7fd41
Bruno Pezman <brunopezman@gmail.com>
2022-12-20 22:41:59 -0300

    DOM

--- js/main.js
+++ js/main.js
@@ -1,6 +1,3 @@
-// Primer mensaje de página
-alert("Bienvenido a Rock Merch N'Roll" )
-
 // Simulador de compra de productos
 
 // array de productos
@@ -200,7 +197,7 @@ const calcularEnvio = (precioTotal) => {
     }
 
     return precioTotal;
-}
+};
 
 // Finalizar la compra
 
@@ -219,6 +216,35 @@ const finalizarCompra = (listaProductos) => {
         +"\n\nLo pagará en "+cuotas+" cuota/s de $"+valorCuota.toFixed(2) 
         +"\n\nGracias por confiar en nosotros!"
     )
-}
+};
 
 comprar();
+
+// Pintar productos
+
+const pintarProductos = () => { 
+    const productosDestacados = document.getElementById('productosDestacados');
+
+    productos.forEach( producto => {
+        const div = document.createElement('div')
+        div.classList.add('product', 'text-center', 'col-lg-3', 'col-md-4', 'col-12')
+        div.innerHTML += `         
+        <img class="img-fluid mb-3" src="${producto.img}" alt="${producto.descripcion}"> 
+        <div class="star">
+            <i class='bx bxs-star warning' ></i>
+            <i class='bx bxs-star warning' ></i>
+            <i class='bx bxs-star warning' ></i>
+            <i class='bx bxs-star warning' ></i>
+            <i class='bx bxs-star warning' ></i>
+        </div>
+        <h5 class="product-name">${producto.nombre}</h5>
+        <h4 class="product price">$${producto.precio}</h4>
+        <button class="buy-btn">Comprar</button>
+        `
+        productosDestacados.appendChild(div)
+    })
+};
+
+pintarProductos();
+
+
--- js/stock.js
+++ js/stock.js
@@ -4,6 +4,7 @@ const productos = [
         id: 1,
         nombre: "Remera The Beatles",
         tipo: "remera",
+        img: "../img/remerathebeatles.png",
         descripcion: "The Beatles - negra - lisa",
         precio: 4000,
         talle: "L",
@@ -13,6 +14,7 @@ const productos = [
         id: 2,
         nombre: "Remera AC/DC",
         tipo: "remera",
+        img: '../img/remeraacdc.png',
         descripcion: "AC/DC - Negra - lisa",
         precio: 4000,
         talle: "L",
@@ -21,6 +23,7 @@ const productos = [
     {
         id:3,
         nombre: "Remera Led Zepellin",
+        img: '../img/remeraledzepellin.png',
         tipo: "remera",
         descripcion:"Led Zeppelin - negra - estampado",
         precio: 4500,
@@ -31,6 +34,7 @@ const productos = [
     {
         id:4,
         nombre: "Remera Bon Jovi",
+        img: '../img/remerabonjovi.png',
         tipo: "remera",
         descripcion:"Bon Jovi - azul marino - lisa",
         precio: 4000,
@@ -40,6 +44,7 @@ const productos = [
     {
         id:5,
         nombre: "Remera Pink Floyd",
+        img:  '../img/remerapinkfloyd.png',
         tipo: "remera",
         descripcion:"Pink Floyd - gris oscuro - estampado",
         precio: 4500,
@@ -49,6 +54,7 @@ const productos = [
     {
         id:6,
         nombre: "Remera Pink Floyd B",
+        img:  '../img/remerapinkfloydb.png',
         tipo: "remera",
         descripcion:"Pink Floyd - negra - estampado",
         precio: 4500,
@@ -59,6 +65,7 @@ const productos = [
     {
         id:7,
         nombre: "Remera The Doors",
+        img:  '../img/remeradoors.png',
         tipo: "remera",
         descripcion:"The Doors - negra - estampado",
         precio: 4500,
@@ -69,6 +76,7 @@ const productos = [
     {
         id:8,
         nombre: "Remera KISS B",
+        img:  '../img/remerakissb.jpg',
         tipo: "remera",
         descripcion:"KISS - negra",
         precio: 4500,
@@ -78,6 +86,7 @@ const productos = [
     {
         id:9,
         nombre: "Remera Aerosmith W",
+        img:  '../img/remeraaerosmithw.jpeg',
         tipo: "remera",
         descripcion:"Aerosmith - blanca - estampado",
         precio: 4500,
@@ -87,6 +96,7 @@ const productos = [
     {
         id:10,
         nombre: "Remera queen W",
+        img:  '../img/remeraqueenw.jpeg',
         tipo: "remera",
         descripcion:"queen - blanca - estampado",
         precio: 4500,
@@ -96,6 +106,7 @@ const productos = [
     {
         id:11,
         nombre: "Remera Los Ramones R",
+        img:  '../img/remeralosramonesr.jpg',
         tipo: "remera",
         descripcion:"Los Ramones - roja - estampado",
         precio: 4500,
@@ -106,6 +117,7 @@ const productos = [
     {
         id:12,
         nombre: "Buzo AC/DC",
+        img:  '../img/busoacdc.png',
         tipo: "buzo",
         descripcion:"AC/DC - negro - liso",
         precio: 4000,
@@ -115,6 +127,7 @@ const productos = [
     {
         id:13,
         nombre: "Buzo Rolling Stones",
+        img:  '../img/busorollingstones.jpg',
         tipo: "buzo",
         descripcion:"Rolling Stones - negro con gris",
         precio: 5000,
@@ -123,7 +136,8 @@ const productos = [
     },
     {
         id:14,
-        nombre: "Gorro Nirvana",
+        nombre: "Gorra Nirvana",
+        img: '../img/gorranirvana.jpg',
         tipo: "accesorio",
         descripcion:"Nirvana - blanco - estampado",
         precio: 1500,
@@ -132,6 +146,7 @@ const productos = [
     {
         id:15,
         nombre: "Gorro Red Hot Chili Peppers",
+        img:  '../img/gorraredhotchilipeppers.jpg',
         tipo: "accesorio",
         descripcion:"Red Hot Chili Peppers - blanca con negro - estampado",
         precio: 1500,
@@ -140,6 +155,7 @@ const productos = [
     {
         id:16,
         nombre: "Chop Queen",
+        img:  '../img/chopqueen.jpg',
         tipo: "vaso",
         descripcion:"Chop Queen - blanco",
         precio: 1500,
@@ -147,7 +163,8 @@ const productos = [
     },
     {
         id:17,
-        nombre: "taza AC/DC",
+        nombre: "Taza AC/DC",
+        img: '../img/tazaacdc.jpg',
         tipo: "vaso",
         descripcion:"taza AC/DC - negra- estampado",
         precio: 1500,
