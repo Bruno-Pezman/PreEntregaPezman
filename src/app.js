1d2b8f28c25269757a4e55e18e3dcc4b833ed4fc
Bruno Pezman <114705198+Bruno-Pezman@users.noreply.github.com>
2023-01-10 18:57:40 -0300

    probando cambios en github2

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
-// Pintar productos en la página
+// Pintar productos en el shop
 
 const pintarProductos = async () => { 
     
-    const productosDestacados = document.getElementById('productosDestacados');
+    const response = await fetch('../src/components/data/stock.json')
+    const data = await response.json()
 
-    const productos = await homeController() 
-    
-    productos.forEach( producto => {
+    const productosDestacados = document.getElementById('productosDestacados');    
+    data.forEach( producto => {
         const div = document.createElement('div');
         div.classList.add('product', 'text-center', 'col-lg-3', 'col-md-4', 'col-12');
         div.innerHTML += `         
