f27f72d48595ff950bd4bee98b4beb400a8a01ec
Bruno Pezman <114705198+Bruno-Pezman@users.noreply.github.com>
2023-01-11 18:52:16 -0300

    .

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    pintarProductos();
-
+    
     if (localStorage.getItem('carrito')) {
         // Invoco a las funciones de carrito.js
         carrito = obtenerCarritoStorage();
