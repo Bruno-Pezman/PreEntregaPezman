8a9fc775808985ff2a85690103f305d586fab480
Bruno Pezman <114705198+Bruno-Pezman@users.noreply.github.com>
2023-01-11 20:55:49 -0300

    .

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//     // const url = '../data/stock.json'
-//     // fetch(url)
-//     // .then(res => res.json()) 
-//     // .then(data => pintarProductos(data))
+document.addEventListener('DOMContentLoaded', () => {
+    const url = '../data/stock.json'
+    fetch(url)
+    .then(res => res.json()) 
+    .then(data => pintarProductos(data))
     
-//     // if (localStorage.getItem('carrito')) {
+    if (localStorage.getItem('carrito')) {
        
-//     //     carrito = obtenerCarritoStorage();
-//     //     actualizarCarrito(carrito);
-//     //     actualizarTotalCarrito(carrito);
-//     // }
-// });
+        carrito = obtenerCarritoStorage();
+        actualizarCarrito(carrito);
+        actualizarTotalCarrito(carrito);
+    }
+});
 
