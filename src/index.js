d26a2956b2b605de830a67541e50dc4b5cf836e4
Bruno Pezman <114705198+Bruno-Pezman@users.noreply.github.com>
2023-01-11 19:46:24 -0300

    .

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ document.addEventListener('DOMContentLoaded', () => {
     fetch(url)
     .then(res => res.json()) 
     .then(data => pintarProductos(data))
-    console.log(data)
-    // if (localStorage.getItem('carrito')) {
-    //     // Invoco a las funciones de carrito.js
-    //     carrito = obtenerCarritoStorage();
-    //     actualizarCarrito(carrito);
-    //     actualizarTotalCarrito(carrito);
-    // }
+    
+    if (localStorage.getItem('carrito')) {
+       
+        carrito = obtenerCarritoStorage();
+        actualizarCarrito(carrito);
+        actualizarTotalCarrito(carrito);
+    }
 });
 
